6c78a75ae8ea9c2f8778d1f4dce0fc5868137a67
BonyChops <office.kentarou@gmail.com>
2020-09-07 10:39:15 +0900

    Edit

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 const MeCab = new require('mecab-async');
 const mecab = new MeCab();
-console.log(mecab.parseSync("読めねえよ"))
-console.log(mecab.parseSync("読めねえよ")[0][6]);
+console.log(mecab.parseSync("ツイート"))
+console.log(mecab.parseSync("ツイート")[0][8]);
